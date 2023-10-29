b8457f180021ec2618f65fc7e3ecbe8ffc23795d
bodokaso <rodrigosantial@gmail.com>
2023-10-29 12:56:04 -0400

    Add project files.

---
+++ Cheques_Integracion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cheques_Integracion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
